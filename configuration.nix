e794c5fde52ede973b19ef0e9063ace1c801af62
bobabree <bree.huynh@berkeley.edu>
2025-11-15 19:43:56 -0800

    Initial NixOS-Cachy config

---
+++ configuration.nix
@@ -0,0 +1,77 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride { mArch = "GENERIC_V3"; };
+  services.scx.enable = true; 
+
+  # Networking options
+  networking.hostName = "bree";
+  networking.networkmanager.enable = true;  
+
+  # Time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Enable sound.
+  services.pulseaudio.enable = false;
+  # OR
+  services.pipewire = {
+     enable = true;
+     alsa.enable = true;
+     alsa.support32Bit = true;
+     pulse.enable = true;
+  };
+  security.rtkit.enable = true;
+
+  # Enable touchpad support
+  services.libinput.enable = true;
+
+  # Define user account
+  users.users.bree = { 
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; 
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+     alacritty
+     fish
+     # firefox_nightly
+     # fuzzel
+     git
+     # niri
+     # noctalia-shell
+     # swaybg
+     vim
+     # xwayland-satellite
+   ];
+
+  fonts.packages = with pkgs; [
+     nerd-fonts.jetbrains-mono
+  ];
+
+  programs.fish.enable = true;
+  users.users.bree.shell = pkgs.fish;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+	description = "Bree";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+	};
+
+	outputs = { nixpkgs, chaotic, ... }: {
+		nixosConfigurations = {
+			bree = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./configuration.nix
+					chaotic.nixosModules.default
+				];
+			};
+		};
+	};
+}
